dd4dbe9dc04cee5d7883d87640ce6fa1563d6a21
Fernando Hurtado <rockfercho@gmail.com>
2024-11-01 03:15:47 -0400

    remove scurity in endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,10 @@ const login = require('./login');
 const middleware = require('../commons/middleware');
 
 function routes (app) {
-
-  app.use('/api/user', middleware.verifyToken, user);
-  app.use('/api/client', middleware.verifyToken, client);
-  app.use('/api/product', middleware.verifyToken, product);
-  app.use('/api/sale', middleware.verifyToken, sale);
+  app.use('/api/user', user);
+  app.use('/api/client', client);
+  app.use('/api/product', product);
+  app.use('/api/sale', sale);
   app.use('/api/login', login);
 }
 
